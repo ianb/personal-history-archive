1a3ea376784d7b4e51e0ea9fa8daabb4fae3c8e9
Ian Bicking <ianb@colorstudy.com>
2018-02-13 16:27:30 -0600

    Add lazygetter helper for getting data

---
+++ python/pha/lazygetter.py
@@ -0,0 +1,25 @@
+import os
+import shutil
+from urllib.request import urlopen
+
+def lazyget(url, filename):
+    if os.path.exists(filename):
+        if os.path.getsize(filename):
+            print("File", filename, "already exists")
+            return
+        else:
+            print("File", filename, "is empty; overwriting")
+    dirname = os.path.dirname(filename)
+    if not os.path.exists(dirname):
+        print("Creating directory %s/" % dirname)
+        os.makedirs(dirname)
+    with urlopen(url) as resp:
+        try:
+            length = int(resp.getheader("Content-Length")) // 1000
+            length = "%skb" % length
+        except:
+            length = "unknown size"
+        print("Reading %s into %s..." % (length, filename), end="")
+        with open(filename, "wb") as fp:
+            shutil.copyfileobj(resp, fp)
+        print(" done.")
