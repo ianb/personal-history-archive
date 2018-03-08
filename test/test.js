917d8d324726ef2951ed3aea168b6361b7d95ada
Ian Bicking <ianb@colorstudy.com>
2018-03-08 16:12:06 -0600

    Verify results in the test

--- test/test.js
+++ test/test.js
@@ -60,17 +60,22 @@ function promiseFinally(promise, finallyCallback) {
   });
 }
 
+function promiseTimeout(time) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, time);
+  });
+}
+
 describe("Test history collection", function() {
   this.timeout(120000);
   let driver;
 
-  before(function() {
-    return getDriver().then((aDriver) => {
-      driver = aDriver;
-    });
+  before(async function() {
+    driver = await getDriver();
   });
 
   after(function() {
+    console.log("running after() now...");
     if (!process.env.NO_CLOSE) {
       return driver.quit();
     }
@@ -79,12 +84,16 @@ describe("Test history collection", function() {
   });
 
   it("will browse about", async function() {
-    this.timeout(15000);
+    let query = "minneapolis wikipedia";
+    let queryDest = "https://en.wikipedia.org/wiki/Minneapolis";
+    this.timeout(60000);
     let driver = await getDriver();
+    // Give the add-on a moment to load:
+    await promiseTimeout(1000);
     await driver.get("https://google.com");
-    await driver.findElement(By.name('q')).sendKeys('webdriver\n');
+    await driver.findElement(By.name('q')).sendKeys(query + "\n");
     //await driver.findElement(By.name('btnK')).click();
-    await driver.wait(until.titleIs('webdriver - Google Search'));
+    await driver.wait(until.titleIs(`${query} - Google Search`));
     await driver.wait(until.elementLocated(By.css(".r a")));
     await driver.findElement(By.css(".r a")).click();
     await driver.wait(async () => {
@@ -96,6 +105,8 @@ describe("Test history collection", function() {
     let mod = process.platform == "darwin" ? Key.COMMAND : Key.CONTROL;
     let selectLinkOpeninNewTab = Key.chord(mod, Key.RETURN);
     await driver.findElement(By.css(".r a")).sendKeys(selectLinkOpeninNewTab);
+    // We want to be sure the Cmd+click opens a tab before we do the next step:
+    await promiseTimeout(1000);
     await driver.get(`${SERVER}/debug.html`);
     await driver.wait(until.elementLocated(By.css("textarea")));
     let result = await driver.findElement(By.css("textarea")).getAttribute("value");
@@ -108,16 +119,75 @@ describe("Test history collection", function() {
     function idToIndex(id) {
       return pages.map(p => p.id).indexOf(id);
     }
-    // FIXME: verify these values!
-    console.log("loadTime:", pages.map(p => p.loadTime));
+    function property(name) {
+      return pages.map(p => p[name]);
+    }
     let urls = pages.map(p => p.url);
+    console.log("Verifying urls,", urls);
+    let expectedUrls = [
+      'about:blank',
+      'https://www.google.com/',
+      /^https:\/\/www.google.com\/search\?.*q=minneapolis/,
+      queryDest,
+      /^https:\/\/www.google.com\/search\?.*q=minneapolis/,
+      queryDest,
+      queryDest, // FIXME: why does this show up twice here?
+      'http://localhost:11180/debug.html',
+    ];
+    assert.equal(urls.length, expectedUrls.length);
+    for (let i=0; i<urls.length; i++) {
+      if (expectedUrls[i] instanceof RegExp) {
+        assert(expectedUrls[i].test(urls[i]), `Bad URL: ${urls[i]} does not match ${expectedUrls[i]}`);
+      } else {
+        assert.equal(urls[i], expectedUrls[i]);
+      }
+    }
     console.log("urls:", urls);
-    console.log("from_address_bar", pages.map(p => p.from_address_bar));
-    console.log("forward_back:", pages.map(p => p.forward_back));
-    console.log("transitionType:", pages.map(p => p.transitionType));
-    console.log("previous:", pages.map(p => idToIndex(p.previousId)));
-    console.log("newTab:", pages.map(p => p.newTab));
-
+    // Apparently driver.get() doesn't act like from_address_bar
+    assert.deepEqual(property("from_address_bar"), [
+      false, false, false, false, false, false, false, false,
+    ]);
+    // We went "back" to the 4th item (the google search)
+    assert.deepEqual(property("forward_back"), [
+      false, false, false, false, true, false, false, false,
+    ]);
+    assert.deepEqual(property("transitionType"), [
+      'existed_onload',
+      'link',
+      'form_submit', // search result
+      'link', // clicked on search result
+      'link', // clicked on back...?
+      undefined, // apparently open in new window is misunderstood
+      'link', // driver.get looks like link?
+      'link', // I don't understand this entry at all
+    ]);
+    assert.deepEqual(pages.map(p => idToIndex(p.previousId)), [
+      -1, // Didn't come from anywhere, about:blank
+      0, // search page
+      1, // search result
+      2, // click on link
+      3, // went "back" to this page... FIXME: is this right?
+      4, // came from previous search result,
+      5, // mysterious extra copy of a page
+      4, // this was opened by the add-on, but appears to come form the search results
+    ]);
+    assert.deepEqual(property("newTab"), [
+      false, false, false, false, false, true, false, false,
+    ]);
+    assert.deepEqual(pages.map(p => !!p.unloadTime), [
+      true, true, true, true, true, true,
+      false, false, // only the last two pages are still loaded
+    ]);
+    assert.deepEqual(property("closedReason"), [
+      'navigation',
+      'navigation',
+      'navigation',
+      'navigation',
+      'navigation',
+      'navigation',
+      null,
+      null, // Only the last two pages haven't been redirected away
+    ]);
     return true;
   });
 
