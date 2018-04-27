01f5377ab0f4444e05141a9c722ae052ae5dcb21
Ian Bicking <ianb@colorstudy.com>
2018-04-27 17:20:05 -0500

    Add a bunch of preferences to the driver setup to help make the requests faster and more predictable, and turn on Tracking Protection

--- test/driver-setup.js
+++ test/driver-setup.js
@@ -9,19 +9,31 @@ exports.getDriver = function(addonFileLocation) {
 
   const options = new firefox.Options()
     .setBinary(firefox.Channel[channel])
-    .setPreference("extensions.legacy.enabled", true)
+    //.setPreference("extensions.legacy.enabled", true)
+    // Let our unsigned add-on be installed:
     .setPreference("xpinstall.signatures.required", false)
+    // Try to keep audio from playing (doesn't work):
     .setPreference("dom.webaudio.enabled", false)
+    // Try to keep videos from auto-playing (doesn't work that well):
     .setPreference("media.autoplay.enabled", false)
+    // Don't let pages do something before unloading:
     .setPreference("dom.disable_beforeunload", true)
+    // Automatically deny all these permission prompts:
     .setPreference("permissions.default.camera", 2)
     .setPreference("permissions.default.desktop-notification", 2)
     .setPreference("permissions.default.geo", 2)
     .setPreference("permissions.default.microphone", 2)
     .setPreference("permissions.default.shortcuts", 2)
+    // Don't let pages make popups:
     .setPreference("capability.policy.default.Window.alert", "noAccess")
     .setPreference("capability.policy.default.Window.confirm", "noAccess")
-    .setPreference("capability.policy.default.Window.prompt", "noAccess");
+    .setPreference("capability.policy.default.Window.prompt", "noAccess")
+    // Tracking protection blocks some nice thing to block:
+    .setPreference("privacy.trackingprotection.enabled", true)
+    .setPreference("privacy.trackingprotection.introCount", 20)
+    // Time out requests after 20 seconds:
+    .setPreference("network.http.response.timeout", 20)
+    .setPreference("network.http.connection-timeout", 20);
 
   const driver = new webdriver.Builder()
     .withCapabilities({"moz:webdriverClick": true})
